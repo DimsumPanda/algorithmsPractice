5961e3ae8d323a7d3a370ef380a527a8f26c7675
Barbara Poon <barbara.poon@nielsen.com>
2016-09-17 12:01:16 -0400

    First commit

---
+++ src/main/BinarySearch.java
@@ -0,0 +1,51 @@
+package main;
+
+
+public class BinarySearch {
+
+	public static void main(String args[]){
+		int[] primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+		
+		int x = 67;
+		int result = doSearch(primes, x);
+		
+		printStatements(primes, result);
+		
+	}
+	
+
+	public static int doSearch(int[] array, int target){
+		int min = 0;
+		int max = array.length-1;
+		
+		while (max >= min){
+			
+		int guess = (min+max)/2;	
+		if (array[guess] == target){
+			return guess;
+		} else if (array[guess] < target){
+			min = guess + 1;
+		} else if (array[guess] > target){
+			max = guess - 1;
+		}
+		}
+		return -1;
+	}
+	
+	public static boolean isPrime(int prime){
+		if (prime == -1){
+			return false;
+		} else return true;
+	}
+	
+	public static void printStatements(int[] array, int number){
+		
+		if (isPrime(number)){
+		System.out.println("The target number is " + array[number]);
+		System.out.println("The index of the target number is at " + number);
+		}
+		else {
+			System.out.println("The number is not prime");
+		}
+	}
+}
---
+++ src/test/TestBinarySearch.java
@@ -0,0 +1,26 @@
+package test;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import main.BinarySearch;
+public class TestBinarySearch {
+
+	@Test
+	public void testBinarySearch(){
+		
+		int[] byFives = {5,10,15,20,25,30,35,40,45};
+		int target = 25;
+		
+		assertEquals(4, BinarySearch.doSearch(byFives, target));
+	}
+	
+	@Test
+	public void testBinarySearch2(){
+		
+		int[] byFives = {5,10,15,20,25,30,35,40,45};
+		int target = 25;
+		
+		assertEquals(3, BinarySearch.doSearch(byFives, target));
+	}
+	
+}
